a53fcb7ac6e120437344e9f66c41fede77db3951
redempta12 <uredempta7@gmail.com>
2024-03-15 01:33:26 +0200

    my commit

--- index.js
+++ index.js
@@ -1,3 +1,74 @@
+var nameError = document.getElementById('name-error');
+var emailError = document.getElementById('email-error');
+var subjectError = document.getElementById('subject-error');
+var messageError = document.getElementById('message-error');
+var submitError = document.getElementById('submit-error');
+
+function validateName() {
+    var name = document.getElementById('full-name').value;
+     if(name.length == 0) {
+        nameError.innerHTML = 'name is required';
+        return false;
+     }
+     if(!name.match(/^[A-Za-z]*\s{1}[A-Za-z]*$/)){
+        nameError.innerHTML = 'write full name';
+        return false; 
+     }
+      nameError.innerHTML = ''; 
+      return true;          
+}
+function validateEmail() {
+    var email = document.getElementById('contact-email').value; 
+    if(email.length == 0) {
+        emailError.innerHTML = 'email is required';
+        return false;
+    }
+    if(!email.match(/^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/)) {
+        emailError.innerHTML = 'email invalid';
+        return false; 
+    }
+    emailError.innerHTML = '';
+        return true;
+}
+
+function validateSubject() {
+    var subject = document.getElementById('contact-subject').value;
+     if(subject.length == 0) {
+        subjectError.innerHTML = 'subject is required';
+        return false;
+     }
+     if(!subject.match(/^[A-Za-z]*\s{1}[A-Za-z]*$/)){
+        nameError.innerHTML = '';
+        return false; 
+     }
+      subjectError.innerHTML = ''; 
+      return true; 
+    }
+
+    function validateMessage() {
+        var message = document.getElementById('contact-message').value;
+        var required = 30;
+        var left = required - message.length;
+
+        if (left > 0) {
+            messageError.innerHTML = left + 'more character required';
+            return false;
+        }
+            messageError.innerHTML = '';
+            return true;
+       
+    }
+function validateForm() {
+    if(!validateName() || validateEmail() || !validateSubject() || !validateMessage()) {
+        submitError.style.display = 'block';
+      submitError.innerHTML = 'please fix error to submit'; 
+      setTimeout(function(){submitError.style.display = 'none';}, 3000);
+      return false;  
+    }
+}
+
+
+
 function openModal() {
 var modal = document.getElementById("lightboxModal");
 var modalImage = document.getElementById("modalImage");
@@ -36,25 +107,4 @@ document.addEventListener('DOMContentLoaded', function()
         duration: 1000,
         easing: 'ease-in-out'
     });
-})
-function validateForm(){
-    var fullname =document.getElementById('name').value
-    var  email =document.getElementById('email').value
-    var  subject =document.getElementById('subject').value
-    var  textarea =document.getElementById('textarea').value
-    var emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-
-if(name.trim() === '') {
-    alert('please enter youn name.');
-    return false;
-}
-if(!emailRegex.test(email)) {
-    alert('please enter a valid email address.');
-    return false;
-}
-if(textarea.trim() === '') {
-    alert('please enter your message.');
-    return false;
-}
-return true;
-}
+});
