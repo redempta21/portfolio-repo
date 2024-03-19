7c3b8b5368aee58cf58fcdf2016fc15d228ec28e
redempta12 <uredempta7@gmail.com>
2024-03-20 01:38:12 +0200

    my commit

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 var nameError = document.getElementById('name-error');
 var emailError = document.getElementById('email-error');
 var subjectError = document.getElementById('subject-error');
@@ -10,7 +11,7 @@ function validateName() {
         nameError.innerHTML = 'name is required';
         return false;
      }
-     if(!name.match(/^[A-Za-z]*\s{1}[A-Za-z]*$/)){
+     if(!name.match(/^[A-Za-z]*\s{  1}[A-Za-z]*$/)){
         nameError.innerHTML = 'write full name';
         return false; 
      }
